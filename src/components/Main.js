888ffb8614178d76ada1090e8189e7b352b65e5a
Mathias Escobar <mathias.escobar.jf@gmail.com>
2025-10-05 17:39:41 -0300

    Adding the form

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,73 @@ import './Main.css';
 function Main(){
     return(
         <main>
-            Este es el main
+            <form>
+                <section>
+                    <h3>Personal details</h3>
+                    <div className='field'>
+                        <input type='text' placeholder='First Name' />
+                    </div>
+                    <div className='field'>
+                        <input type='text' placeholder='Last Name' />
+                    </div>
+                    <div className='field'>
+                        <input type='text' placeholder='Email' />
+                    </div>
+                    <div className='field'>
+                        <input type='text' placeholder='Telephone Number' />
+                    </div>
+                </section>
+                <section>
+                    <h3>Booking details</h3>
+                    {/* Agregar condicion de formato */}
+                    <div className='field'>
+                        <input type='text' placeholder='Date  mm/dd' />
+                    </div>
+                    {/* Agregar condicion de formato */}
+                    <div className='field'>
+                        <input type='text' placeholder='Time hh:00 o hh:30' />
+                    </div>
+                    <div>
+                        <select>
+                            <option disabled selected>Occasion</option>
+                            <option>Birthday</option>
+                            <option>Engagement</option>
+                            <option>Anniversary</option>
+                            <option>Other</option>
+                        </select>
+                    </div>
+                    <fieldset>
+                        <legend>Seating options</legend>
+                        <div className='field'>
+                            <input type='radio' name='Seating options' value='Indoor' />
+                        </div>
+                        <div className='field'>
+                            <input type='radio' name='Seating options' value='Outdoor' />
+                        </div>
+                    </fieldset>
+                </section>
+                <section>
+                    <h3>Confirmation details</h3>
+                    <fieldset>
+                        <legend>Confirmation method</legend>
+                        <div className='field'>
+                            <label>
+                                <input type='radio' name='Confirmation method' value='Send me booking confirmation via text' /> Send me booking confirmation via text
+                            </label>
+                        </div>
+                        <div className='field'>
+                            <input type='radio' name='Confirmation method' value='Send me booking confirmation via email' />
+                        </div>    
+                    </fieldset>
+                </section>
+                <section>
+                    <h3>Extra details</h3>
+                    <div className='field'>
+                        <textarea placeholder='Write a comment (optional)'></textarea>
+                    </div>
+                </section>
+                <button type="submit">Reserve the table</button>
+            </form>
         </main>
     );
 };
